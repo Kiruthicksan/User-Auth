9286da3cec31254b32f6e6a1dd0786995a8aa77c
Kiruthicksan <kiruthicksanboopathi@gmail.com>
2025-08-27 11:28:36 +0530

    Intialized server

---
+++ backend/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
